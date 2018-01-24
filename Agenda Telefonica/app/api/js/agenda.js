6f4210874bff581ce20e2001c853506034110cfd
Tiago Pereira Coelho <tiago.pereira@FORLOGIC.LOCAL>
2018-01-24 09:48:10 -0200

    Botão Editar

--- Agenda Telefonica/app/api/js/agenda.js
+++ Agenda Telefonica/app/api/js/agenda.js
@@ -46,4 +46,32 @@ $(document).ready(function() {
 			alert(status);
 		});
 	});
+	$('#editar').click(function () {
+		alert('Editando!');
+		var id = $('#id').val();
+		var nome = $('#nome').val();
+		var email = $('#email').val();
+		var sexo = $('#sexo').val();
+		var endereco = $('#endereco').val();
+		var obs = $('#obs').val();
+		alert('Informações capturadas!');
+		let data = {
+				firstName: nome,
+				email: email,
+				gender: sexo,
+				info: {
+	                address: endereco,
+	                comments: obs
+	            }
+	    }
+	    alert("Informações armazenadas em data");
+		$.ajax({
+		  url: `http://localhost:3000/v1/contacts/${id}`,
+		  type: 'PUT',
+		  data: data,
+		  success: function(data) {
+		    alert("Alterações realizadas!");
+		  }
+		});
+	});
 });
